93d9c33512055a0986a799dd2ebf52232d7960c0
Rashedul Haydar <rh2712@columbia.edu>
2020-11-29 11:13:21 -0500

    Initial commit
    Single fragment with placeholder title and description.

---
+++ app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -0,0 +1,23 @@
+package com.hfad.workout;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WorkoutDetailFragment extends Fragment {
+
+    public WorkoutDetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+}
