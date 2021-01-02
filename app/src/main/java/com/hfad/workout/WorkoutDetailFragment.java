471401148c6fec87cde54b75317788ec81ec5e75
Rashedul Haydar <rh2712@columbia.edu>
2021-01-01 22:56:07 -0500

    Display a specific workout in workout detail fragment.

--- app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class WorkoutDetailFragment extends Fragment {
+    private long workoutId;
 
     public WorkoutDetailFragment() {
         // Required empty public constructor
@@ -19,5 +21,22 @@ public class WorkoutDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            Workout workout = Workout.workouts[(int)workoutId];
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            title.setText(workout.getName());
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
+    }
+
+    public void setWorkout(long id) {
+        this.workoutId = id;
     }
 }
