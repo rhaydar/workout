bda4f9580b30c14b607932ebbca3a1541957f49a
Rashedul Haydar <rh2712@columbia.edu>
2021-01-02 21:47:48 -0500

    Change details button to list of workout names.
    The button on the main activity page is replaced with a list
    fragment that shows the names of the workouts.

---
+++ app/src/main/java/com/hfad/workout/WorkoutListFragment.java
@@ -0,0 +1,34 @@
+package com.hfad.workout;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+
+public class WorkoutListFragment extends ListFragment {
+
+    public WorkoutListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        String[] names = new String[Workout.workouts.length];
+        for (int i=0; i<names.length; i++) {
+            names[i] = Workout.workouts[i].getName();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                inflater.getContext(), android.R.layout.simple_list_item_1,
+                names);
+        setListAdapter(adapter);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
