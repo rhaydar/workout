f6cdd88698faabd8eae445a0d5c056b90f4009ff
Rashedul Haydar <rh2712@columbia.edu>
2021-01-06 22:43:39 -0500

    Ch. 11: Temp activity supports rotation.
    By using a frame layout and dynamically creating an instance of a
    StopwatchFragment if one doesn't exist already, and using fragment
    transaction, we can retain the state of the stopwatch fragment.

--- app/src/main/java/com/hfad/workout/TempActivity.java
+++ app/src/main/java/com/hfad/workout/TempActivity.java
@@ -3,6 +3,7 @@ package com.hfad.workout;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 public class TempActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class TempActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
+        if (savedInstanceState == null) {
+            StopwatchFragment stopwatch = new StopwatchFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.stopwatch_container, stopwatch);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        }
     }
 }
